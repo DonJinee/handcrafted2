091ab0f394fb9090084e2b7378651f0b08ddf749
Laurels Echichinnwo <laurels.echichinwo@stu.cu.edu.ng>
2023-12-04 21:18:27 +0000

    authchecker added

--- handcrafted/src/pages/_app.tsx
+++ handcrafted/src/pages/_app.tsx
@@ -1,12 +1,61 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import React, { Fragment } from 'react';
-// import { ProvideAuth } from "../components/context/AuthContext";
+import React, { Fragment, useState, useEffect, useCallback } from 'react';
+import { useRouter } from 'next/router';
+import { userService } from '@/services/user.service';
+import Head from 'next/head';
+
+interface User {
+  id: string;
+  token?: string;
+  // Add other properties as needed
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const [authorized, setAuthorized] = useState(false);
+
+  const authCheck = useCallback((url: string) => {
+    // redirect to login page if accessing a private page and not logged in 
+    setUser(userService.userValue);
+    const publicPaths = ['/signin', '/register'];
+    const path = url.split('?')[0];
+    if (!userService.userValue && !publicPaths.includes(path)) {
+      setAuthorized(false);
+      router.push({
+        pathname: '/signin',
+        query: { returnUrl: router.asPath }
+      });
+    } else {
+      setAuthorized(true);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    // on initial load - run auth check 
+    authCheck(router.asPath);
+
+    // on route change start - hide page content by setting authorized to false  
+    const hideContent = () => setAuthorized(false);
+    router.events.on('routeChangeStart', hideContent);
+
+    // on route change complete - run auth check 
+    router.events.on('routeChangeComplete', () => authCheck(router.asPath));
+
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off('routeChangeStart', hideContent);
+      router.events.off('routeChangeComplete', () => authCheck(router.asPath));
+    };
+  }, [authCheck, router]);
+
   return (
     <Fragment>
-      <Component {...pageProps} />
-      </Fragment>
-  )
+      <Head>
+        <title>Hand-Crafted Haven</title>
+      </Head>
+      {authorized && <Component {...pageProps} />}
+    </Fragment>
+  );
 }
